d69c24c223627124a83053d15017992cb0ca7742
Arshia-Izadyar <arshiaa104@gmail.com>
2023-11-08 15:19:04 +0330

    updated validator

--- utils/rate_validator.py
+++ utils/rate_validator.py
@@ -1,7 +1,13 @@
 from django.core.exceptions import ValidationError
-
+from datetime import datetime
 
 
 def validate_rate(rate):
     if rate > 5 or rate < 0:
         raise ValidationError("rate should be 0-5")
+    
+    
+def validated_date(date):
+    if date < datetime.now().date():
+        raise ValidationError("Invalid Date")
+    
